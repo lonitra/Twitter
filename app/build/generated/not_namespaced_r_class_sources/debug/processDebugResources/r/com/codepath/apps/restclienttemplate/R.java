1bfb04d8b75dbc7a9e4558f10e7c414a90579ba1
Loni Tra <lonitra@fb.com>
2019-07-03 16:07:20 -0700

    Added character count to reply activity

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/codepath/apps/restclienttemplate/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/codepath/apps/restclienttemplate/R.java
@@ -3219,15 +3219,16 @@ public final class R {
     public static final int topPanel=0x7f07009d;
     public static final int tvBody=0x7f07009e;
     public static final int tvBodyReply=0x7f07009f;
-    public static final int tvCount=0x7f0700a0;
-    public static final int tvCreatedAt=0x7f0700a1;
-    public static final int tvUsername=0x7f0700a2;
-    public static final int uniform=0x7f0700a3;
-    public static final int up=0x7f0700a4;
-    public static final int useLogo=0x7f0700a5;
-    public static final int withText=0x7f0700a6;
-    public static final int wrap=0x7f0700a7;
-    public static final int wrap_content=0x7f0700a8;
+    public static final int tvCharactersLeft=0x7f0700a0;
+    public static final int tvCount=0x7f0700a1;
+    public static final int tvCreatedAt=0x7f0700a2;
+    public static final int tvUsername=0x7f0700a3;
+    public static final int uniform=0x7f0700a4;
+    public static final int up=0x7f0700a5;
+    public static final int useLogo=0x7f0700a6;
+    public static final int withText=0x7f0700a7;
+    public static final int wrap=0x7f0700a8;
+    public static final int wrap_content=0x7f0700a9;
   }
   public static final class integer {
     public static final int abc_config_activityDefaultDur=0x7f080000;
--- app/src/main/java/com/codepath/apps/restclienttemplate/ReplyActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ReplyActivity.java
@@ -2,6 +2,8 @@ package com.codepath.apps.restclienttemplate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ public class ReplyActivity extends AppCompatActivity {
     TextView tvBodyReply;
     EditText etReply;
     ImageView ivProfileReply;
+    TextView tvCharactersLeft;
 
 
     @Override
@@ -24,13 +27,30 @@ public class ReplyActivity extends AppCompatActivity {
         tvBodyReply = findViewById(R.id.tvBodyReply);
         etReply = findViewById(R.id.etReply);
         ivProfileReply = findViewById(R.id.ivProfileReply);
+        tvCharactersLeft = findViewById(R.id.tvCharactersLeft);
         Tweet replyTweet = Parcels.unwrap(getIntent().getParcelableExtra("Tweet"));
         tvBodyReply.setText(replyTweet.body);
         etReply.setText("@" + replyTweet.user.screenName + " ");
+        tvCharactersLeft.setText(280 - etReply.getText().length() + " characters left");
         Glide.with(this)
                 .load(replyTweet.user.profileImageUrl)
                 .into(ivProfileReply);
 
+        etReply.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int characters = etReply.getText().length();
+                tvCharactersLeft.setText((280 - s.length()) + " characters left");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
     }
 
